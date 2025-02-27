72e2313314d6a0b63c34eba7af8e9a44204e6649
Yauheni Karaniuk <ykaraniuk@fixnetwork.com>
2025-02-27 16:07:26 -0500

    Final version of Chef Claude app. Currently only works in local env because of API key dependency.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,39 +1,57 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
+import IngredientsList from "./IngredientsList";
+import ClaudeRecipe from "./ClaudeRecipe"
+ import { getRecipeFromMistral } from "../ai"
 
 export default function Main() {
-    const [ingredientList, setIngredientList] = useState([
-        "Beans", 
-        "Some more beans", 
-        "Beans Beans Beans",
-        "Seriously all I got is beans",
+    const [ingredients, setIngredientList] = useState([
+        "Tomato Sauce", 
+        "Lean Ground Beef", 
+        "Common spices",
+        "Fusilli Pasta",
     ]);
 
-    const ingredientListJSX = ingredientList.map(ingredient => (
-        <li key={ingredient}>{ingredient}</li>
-    ));
+    const [recipe, setRecipe] = useState("");
+    const recipeSection = useRef(null);
+    
+    useEffect(() => {
+        console.log(recipeSection);
+        if (recipe !== "" && recipeSection.current !== null) {
+            recipeSection.current.scrollIntoView({behavior: "smooth"});
+        }
+    }, [recipe])
 
-    function addIngredient(event) {
-        event.preventDefault();
-        const formData = new FormData(event.currentTarget);
+    async function getRecipe() {
+        const recipeMarkdown = await getRecipeFromMistral(ingredients);
+        setRecipe(recipeMarkdown);
+    }
+
+    function addIngredient(formData) {
         const newIngredient = formData.get("ingredientInput");
         setIngredientList(prevIngredientList => [...prevIngredientList, newIngredient]);
     }
 
     return(
         <main>
-            <form className="add-ingredient-form" onSubmit={addIngredient}>
+            <form action={addIngredient} className="add-ingredient-form">
                 <input
                     className="add-ingredient-input"
                     type="text"
-                    placeholder="e.g. Canned beans"
-                    aria-lavel="Add ingredient input"
+                    placeholder="e.g. Chicken thighs"
+                    aria-label="Add ingredient input"
                     name="ingredientInput"
                 />
                 <button className="add-ingredient-btn">Add Ingredient</button>
             </form> 
-            <ul>
-                {ingredientListJSX}
-            </ul>
+            {ingredients.length > 0 &&
+                <IngredientsList
+                    ref={recipeSection}    
+                    ingredients={ingredients}
+                    getRecipe={getRecipe}
+                />
+            }
+
+            {recipe && <ClaudeRecipe recipe={recipe} />}
         </main>
     );
 }
